4a7c432d8889acbb2e5d11aa79a816700159f305
Viken Manoukian <68619882+string-dot-byte@users.noreply.github.com>
2023-10-20 16:22:26 -0400

    Update Prime.hpp
    Removed useless check

--- Util/Prime.hpp
+++ Util/Prime.hpp
@@ -2,7 +2,6 @@
 
 bool isPrime(int num) {
     if (num <= 1) return false;
-    if (num <= 3) return true;
     if (num % 2 == 0 || num % 3 == 0) return false;
     
     for (int i = 5; i * i <= num; i += 6) {
