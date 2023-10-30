9cd0b3bb164c55ac95e7a99b856389ca76f0c7ab
Viken Manoukian <68619882+string-dot-byte@users.noreply.github.com>
2023-10-29 21:32:23 -0400

    Solved

---
+++ Solutions/0046/0046.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+#include "../../Util/Math.hpp"
+
+
+bool validateConjecture(int n) {
+    for (int i = 1; i < n; i += 2) {
+        if (!isPrime(i)) continue;
+
+        for (int j = 1; ; ++j) {
+            const int powe = 2*std::pow(j, 2);
+
+            if (powe > n) break;
+            else if (powe + i == n) return true;
+        }
+    }
+
+    return false;
+}
+
+
+int main() {
+    for (int i = 7; ; i += 2) {
+        if (isPrime(i)) continue;
+
+        if (!validateConjecture(i)) {
+            std::cout << i;
+            break;
+        }
+    }
+
+    return 0;
+}
