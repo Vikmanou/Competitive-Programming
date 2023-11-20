1c181d625f1f659306ae3b77b6f4be9b4578cff1
Manoukian Viken <2379344@crosemont.qc.ca>
2023-11-20 15:41:46 -0500

    I made it faster

--- Solutions/0357/0357.cpp
+++ Solutions/0357/0357.cpp
@@ -2,18 +2,21 @@
 #include "../../Util/Math.hpp"
 
 
+const unsigned long long LIMIT = 100'000'000;
+
 int main() {
-    std::vector<bool> sieve = getSieve(100'000'000);
+    std::vector<bool> sieve = getSieve(LIMIT + LIMIT/2);
 
     unsigned long long ans = 1;
 
-    for (int i = 2; i < 100'000'000; i += 2) {
+    for (unsigned long long i = 2; i < LIMIT; i += 4) {
+        if (!isPrime(1+i) || !isPrime(2 + i/2)) continue;
+
         std::vector<unsigned long long> factorsi = getFactors(i);
 
         bool pass = true;
-        for (int j = 0; j < factorsi.size(); ++j) {
+        for (int j = 2; j < factorsi.size(); ++j) {
             unsigned long long d = factorsi[j];
-            //if ( !isPrime( d + i/d ) ) {
             if (!sieve[d + i/d]) {
                 pass = false;
                 break;
