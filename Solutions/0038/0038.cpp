4c50c15ebf207f30b3063b1ca84ded503df08d20
Manoukian Viken <2379344@crosemont.qc.ca>
2023-12-04 11:48:41 -0500

    Solved 38

---
+++ Solutions/0038/0038.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include "../../Util/Math.hpp"
+
+
+int main() {
+    int largestPandigital = 0;
+
+    for (int i = 1; i < 10000; i++) {
+        std::string concProd;
+        int n = 1;
+
+        while (concProd.length() < 9) {
+            concProd += std::to_string(i * n);
+            n++;
+        }
+
+        long long nConcProd = std::stoll(concProd);
+        if (concProd.length() == 9 && isnPandigital(nConcProd)) {
+            int pandigitalNumber = nConcProd;
+            largestPandigital = std::max(largestPandigital, pandigitalNumber);
+        }
+    }
+
+    std::cout << largestPandigital;
+
+    return 0;
+}
